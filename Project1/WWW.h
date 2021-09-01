d60f330df58a3c5ee0f834c6870856460e90d982
tak <Takkarigomba@gmail.com>
2021-09-01 14:25:07 +0100

    constructor

--- Project1/WWW.h
+++ Project1/WWW.h
@@ -1,17 +1,35 @@
 #pragma once
-#include <iostream>
 #include <fstream>
+#include <iostream>
+using namespace std;
 
-struct SensorV1 { //32 bytes
+struct Wind {
+public:
 	char ID;
 	long Peakpower;
 	double MachineTemp;
 	double windspeed;
 	char speedunits;
-	double date;
-	long time;
+	char date;
 	char region;
 	int hour;
 	int min;
+	int secs;
 	bool isActive;
+
+	Wind( char ID, long Peakpower, double MachineTemp, double windspeed, char speedunits, int hour, int min, int secs, char date, char region, bool isActive){
+		this->ID - ID;
+		this->Peakpower = Peakpower;
+		this->MachineTemp = MachineTemp;
+		this->windspeed = windspeed;
+		this->speedunits = speedunits;
+		this->hour - hour;
+		this->min = min;
+		this->secs = secs;
+		this->date = date;
+		this->region = region;
+		this-> isActive = isActive;
+	}
+
+	
 };
