996124101c04d843d11b09b1ebeff2ffcc17515e
tak <Takkarigomba@gmail.com>
2021-09-03 18:14:32 +0100

    group by region class

--- Project1/WWW.h
+++ Project1/WWW.h
@@ -1,35 +1,27 @@
 #pragma once
 #include <fstream>
 #include <iostream>
+#include <vector>
 using namespace std;
+
 
 struct Wind {
 public:
-	char ID;
-	long Peakpower;
-	double MachineTemp;
-	double windspeed;
-	char speedunits;
-	char date;
-	char region;
-	int hour;
-	int min;
-	int secs;
-	bool isActive;
 
-	Wind( char ID, long Peakpower, double MachineTemp, double windspeed, char speedunits, int hour, int min, int secs, char date, char region, bool isActive){
-		this->ID - ID;
-		this->Peakpower = Peakpower;
-		this->MachineTemp = MachineTemp;
-		this->windspeed = windspeed;
-		this->speedunits = speedunits;
-		this->hour - hour;
-		this->min = min;
-		this->secs = secs;
-		this->date = date;
-		this->region = region;
-		this-> isActive = isActive;
-	}
+	float WindSpeed;
+	float MachineTemp;
+	int PeakPower;
+	int Status;
+	string ID;
+	string Region;
+	string TempUnits;
+	string SpeedUnits;
+	int year, month, day, hour, minute, second;
+};
 
-	
+
+class groupByRegion {
+public:
+	string name;
+	vector<Wind> data;
 };
